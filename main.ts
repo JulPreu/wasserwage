68daa986b778049aa677d51e26da71d553ceb7fa
JulPreu <julian.preuschoff@gmx.net>
2025-05-21 16:31:16 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,12 @@ let neigung_y = 0
 basic.forever(function () {
     neigung_x = input.rotation(Rotation.Pitch)
     neigung_y = input.rotation(Rotation.Roll)
-    if (Math.abs(neigung_x) < 2 && Math.abs(neigung_y) < 2) {
+    if (Math.abs(neigung_x) < 10 && Math.abs(neigung_y) < 10) {
         basic.setLedColors(0x00ff00, 0x00ff00, 0x00ff00)
     } else {
         basic.setLedColors(0xff0000, 0xff0000, 0xff0000)
     }
-    if (neigung_y >= 2) {
+    if (neigung_y >= 10) {
         basic.showLeds(`
             # # . . .
             # # . . .
@@ -17,7 +17,43 @@ basic.forever(function () {
             # # . . .
             `)
     }
-    if (neigung_y < 2) {
+    if (neigung_y < 10) {
+        basic.clearScreen()
+    }
+    if (neigung_y <= -10) {
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            `)
+    }
+    if (neigung_y > -10) {
+        basic.clearScreen()
+    }
+    if (neigung_x >= 10) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (neigung_x < 10) {
+        basic.clearScreen()
+    }
+    if (neigung_x <= -10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (neigung_x > -10) {
         basic.clearScreen()
     }
 })
